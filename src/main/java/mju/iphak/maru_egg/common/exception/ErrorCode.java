92ab36bb0ab6c0113a4865e1f8b381876606f6ec
나경호 <hoyana1225@gmail.com>
2024-07-11 01:11:38 +0900

    refactor: ErrorCode 추가

--- src/main/java/mju/iphak/maru_egg/common/exception/ErrorCode.java
+++ src/main/java/mju/iphak/maru_egg/common/exception/ErrorCode.java
@@ -14,8 +14,8 @@ public enum ErrorCode {
 	// 400 error
 
 	// 404 error
-	NOT_FOUND_CHAT(NOT_FOUND, "id: %s인 채팅방을 찾을 수 없습니다."),
-	NOT_FOUND_CHAT_MESSAGE_IN_CHAT(NOT_FOUND, "id: %s인 채팅방의 채팅을 찾을 수 없습니다.");
+	NOT_FOUND_QUESTION(NOT_FOUND, "type: %s, category: %s, content: %s인 질문을 찾을 수 없습니다."),
+	NOT_FOUND_ANSWER(NOT_FOUND, "질문 id가 %s인 답변을 찾을 수 없습니다.");
 
 	// 500 error
 
