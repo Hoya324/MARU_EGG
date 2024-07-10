9319f03aa4333aa0e27d0b1b37eee45e2264846d
나경호 <hoyana1225@gmail.com>
2024-07-11 01:08:23 +0900

    refactor: Question Repository find 메서드 정의

--- src/main/java/mju/iphak/maru_egg/question/repository/QuestionRepository.java
+++ src/main/java/mju/iphak/maru_egg/question/repository/QuestionRepository.java
@@ -1,8 +1,15 @@
 package mju.iphak.maru_egg.question.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import mju.iphak.maru_egg.question.domain.Question;
+import mju.iphak.maru_egg.question.domain.QuestionCategory;
+import mju.iphak.maru_egg.question.domain.QuestionType;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+	Optional<Question> findByContentAndQuestionCategoryAndQuestionType(final String content,
+		final QuestionCategory questionCategory,
+		final QuestionType questionType);
 }
