b9b07f31afd0cedadff2e943a844a2f579489eac
나경호 <hoyana1225@gmail.com>
2024-07-11 01:38:15 +0900

    refactor: 응답 operation 추가

--- src/main/java/mju/iphak/maru_egg/question/api/QuestionController.java
+++ src/main/java/mju/iphak/maru_egg/question/api/QuestionController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import mju.iphak.maru_egg.question.application.QuestionService;
@@ -19,6 +21,11 @@ public class QuestionController {
 
 	private final QuestionService questionService;
 
+	@Tag(name = "question")
+	@Operation(summary = "질문 요청", description = "질문하는 API", responses = {
+		@ApiResponse(responseCode = "200", description = "질문 성공"),
+		@ApiResponse(responseCode = "404", description = "질문 또는 답변을 찾지 못한 경우"),
+	})
 	@PostMapping()
 	public QuestionResponse question(@RequestBody QuestionRequest request) {
 		return questionService.getQuestionResponse(request.type(), request.category(), request.content());
