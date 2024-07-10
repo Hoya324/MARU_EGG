f4fe653ca2124631c70ac0081c58992dba7b3b13
나경호 <hoyana1225@gmail.com>
2024-07-10 18:17:28 +0900

    refactor: QuestionCategory 수정

--- src/main/java/mju/iphak/maru_egg/question/domain/QuestionCategory.java
+++ src/main/java/mju/iphak/maru_egg/question/domain/QuestionCategory.java
@@ -4,9 +4,9 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum QuestionCategory {
-	ADMISSIONS_TIMELINE("전형 일정"),
-	QUALIFICATION("지원자격"),
-	DOCUMENTATION("제출서류"),
+	ADMISSION_GUIDELINE("모집요강"),
+	PASSING_RESULT("입시결과"),
+	PAST_QUESTIONS("기출문제"),
 	UNIV_LIFE("대학생활"),
 	INTERVIEW_PRACTICAL_TEST("면접/실기"),
 	ETC("기타");
