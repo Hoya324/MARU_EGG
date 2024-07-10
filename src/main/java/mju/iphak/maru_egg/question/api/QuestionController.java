6b0c0b469220deef9980edb424ec53cfba944cf0
나경호 <hoyana1225@gmail.com>
2024-07-11 01:07:56 +0900

    refactor: Question controller 설정

--- src/main/java/mju/iphak/maru_egg/question/api/QuestionController.java
+++ src/main/java/mju/iphak/maru_egg/question/api/QuestionController.java
@@ -1,18 +1,26 @@
 package mju.iphak.maru_egg.question.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import mju.iphak.maru_egg.question.application.QuestionService;
+import mju.iphak.maru_egg.question.dto.request.QuestionRequest;
+import mju.iphak.maru_egg.question.dto.response.QuestionResponse;
 
 @Tag(name = "Question API", description = "질문 관련 API 입니다.")
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/questions")
 public class QuestionController {
 
-	@GetMapping()
-	public String getQuestion() {
-		return "아 됐다. 휴";
+	private final QuestionService questionService;
+
+	@PostMapping()
+	public QuestionResponse question(@RequestBody QuestionRequest request) {
+		return questionService.getQuestionResponse(request.type(), request.category(), request.content());
 	}
 }
