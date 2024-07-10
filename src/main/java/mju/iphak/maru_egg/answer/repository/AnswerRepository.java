cf63258a80c5a79095d6029d158cfb02b7a97e90
나경호 <hoyana1225@gmail.com>
2024-07-11 01:09:50 +0900

    refactor: Answer Repository find 메서드 정의

--- src/main/java/mju/iphak/maru_egg/answer/repository/AnswerRepository.java
+++ src/main/java/mju/iphak/maru_egg/answer/repository/AnswerRepository.java
@@ -1,8 +1,11 @@
 package mju.iphak.maru_egg.answer.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import mju.iphak.maru_egg.answer.domain.Answer;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+	Optional<Answer> findByQuestionId(Long id);
 }
