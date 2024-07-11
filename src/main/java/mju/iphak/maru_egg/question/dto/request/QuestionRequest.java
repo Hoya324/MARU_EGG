36ed35f8d106bbf8ad18d08bb7cc474a766e09b6
나경호 <hoyana1225@gmail.com>
2024-07-11 22:03:31 +0900

    refactor: validation 추가 및 예시 추가

--- src/main/java/mju/iphak/maru_egg/question/dto/request/QuestionRequest.java
+++ src/main/java/mju/iphak/maru_egg/question/dto/request/QuestionRequest.java
@@ -1,10 +1,17 @@
 package mju.iphak.maru_egg.question.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import mju.iphak.maru_egg.question.domain.QuestionCategory;
 import mju.iphak.maru_egg.question.domain.QuestionType;
 
-@Schema(description = "질문 생성 요청 DTO")
+@Schema(description = "질문 생성 요청 DTO", example = """
+	{
+	  "type": "SUSI 또는 PYEONIP 또는 JEONGSI 또는 JAEOEGUGMIN",
+	  "category": "PAST_QUESTIONS 또는 UNIV_LIFE 또는 INTERVIEW_PRACTICAL_TEST 또는 PASSING_RESULT 또는 ADMISSION_GUIDELINE 또는 ETC",
+	  "content": "수시 입학 요강에 대해 알려주세요."
+	}
+	""")
 public record QuestionRequest(
 
 	@Schema(description = "질문 타입(수시, 정시, 편입학, 재외국민)")
@@ -14,6 +21,7 @@ public record QuestionRequest(
 	QuestionCategory category,
 
 	@Schema(description = "질문 내용")
+	@NotBlank(message = "질문은 비어있을 수 없습니다.")
 	String content
 ) {
 }
