e833b14fdf465b8a821531adadf4c01861e1bb9c
나경호 <hoyana1225@gmail.com>
2024-07-11 22:03:40 +0900

    refactor: 예시 추가

--- src/main/java/mju/iphak/maru_egg/question/dto/response/QuestionResponse.java
+++ src/main/java/mju/iphak/maru_egg/question/dto/response/QuestionResponse.java
@@ -6,7 +6,18 @@ import mju.iphak.maru_egg.answer.dto.response.AnswerResponse;
 import mju.iphak.maru_egg.question.domain.Question;
 
 @Builder
-@Schema(description = "질문 응답 DTO")
+@Schema(description = "질문 응답 DTO", example = """
+	{
+	  "id": 1,
+	  "dateInformation": "생성일자: 2023-07-11T10:00:00, 마지막 DB 갱신일자: 2023-07-11T12:00:00",
+	  "answer": {
+	    "id": 1,
+	    "content": "입학 요강은 다음과 같습니다."
+	    "renewalYear" : 2024,
+	    "dateInformation": "생성일자: 2023-07-11T10:00:00, 마지막 DB 갱신일자: 2023-07-11T12:00:00"
+	  }
+	}
+	""")
 public record QuestionResponse(
 	@Schema(description = "질문 id")
 	Long id,
