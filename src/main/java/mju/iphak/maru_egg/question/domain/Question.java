e320af5adf8302be7fd93fa921fb0643bcec9edd
나경호 <hoyana1225@gmail.com>
2024-07-16 00:35:03 +0900

    refactor: question column 추가 및 jpa 설정

--- src/main/java/mju/iphak/maru_egg/question/domain/Question.java
+++ src/main/java/mju/iphak/maru_egg/question/domain/Question.java
@@ -1,6 +1,9 @@
 package mju.iphak.maru_egg.question.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -17,9 +20,19 @@ import mju.iphak.maru_egg.common.entity.BaseEntity;
 @Table(name = "questions")
 public class Question extends BaseEntity {
 
+	@Column(columnDefinition = "TEXT", nullable = false)
 	private String content;
+
+	private String contentToken;
+
+	@Enumerated(EnumType.STRING)
 	private QuestionType questionType;
+
+	@Enumerated(EnumType.STRING)
 	private QuestionCategory questionCategory;
+
+	@Column(name = "view_count", nullable = false)
+	private int viewCount;
 
 	public String getDateInformation() {
 		return "생성일자: %s, 마지막 DB 갱신일자: %s".formatted(this.getCreatedAt(), this.getUpdatedAt());
@@ -28,6 +41,7 @@ public class Question extends BaseEntity {
 	public static Question of(String content, QuestionType questionType, QuestionCategory questionCategory) {
 		return Question.builder()
 			.content(content)
+			.contentToken(contentToken)
 			.questionType(questionType)
 			.questionCategory(questionCategory)
 			.build();
