cfa077944ec4802cdd0d05d4a5ac5bf42dc2f755
나경호 <hoyana1225@gmail.com>
2024-07-19 22:02:08 +0900

    test: QuestionRepositoryTest

--- src/test/java/mju/iphak/maru_egg/question/repository/QuestionRepositoryTest.java
+++ src/test/java/mju/iphak/maru_egg/question/repository/QuestionRepositoryTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import mju.iphak.maru_egg.answer.domain.Answer;
 import mju.iphak.maru_egg.answer.repository.AnswerRepository;
@@ -25,31 +24,26 @@ class QuestionRepositoryTest extends RepositoryTest {
 	@Autowired
 	private AnswerRepository answerRepository;
 
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-
 	private Question question;
 	private Answer answer;
 
 	@BeforeEach
 	public void setUp() throws Exception {
-		jdbcTemplate.execute("ALTER TABLE questions ADD FULLTEXT INDEX idx_ft_question_content(content)");
-
-		question = new Question("테스트 질문입니다.", "테스트 질문", QuestionType.SUSI,
+		question = new Question("테스트 질문 예시 예시입니다.", "테스트 질문 예시", QuestionType.SUSI,
 			QuestionCategory.ADMISSION_GUIDELINE, 0);
 		questionRepository.save(question);
-		Question additionalQuestion1 = Question.of("추가1 테스트 질문입니다.", "추가 테스트 질문", QuestionType.SUSI,
+		Question additionalQuestion1 = Question.of("추가1 테스트 질문 예시입니다.", "추가 테스트 질문 예시", QuestionType.SUSI,
 			QuestionCategory.ADMISSION_GUIDELINE);
 		questionRepository.save(additionalQuestion1);
-		Question additionalQuestion2 = Question.of("추가2 테스트 질문입니다.", "추가 테스트 질문", QuestionType.SUSI,
+		Question additionalQuestion2 = Question.of("추가2 테스트 질문 예시입니다.", "추가 테스트 질문 예시", QuestionType.SUSI,
 			QuestionCategory.ADMISSION_GUIDELINE);
 		questionRepository.save(additionalQuestion2);
-		Question additionalQuestion3 = Question.of("추가3 테스트 질문입니다.", "추가 테스트 질문", QuestionType.SUSI,
+		Question additionalQuestion3 = Question.of("추가3 테스트 질문 예시입니다.", "추가 테스트 질문 예시", QuestionType.SUSI,
 			QuestionCategory.ADMISSION_GUIDELINE);
 		questionRepository.save(additionalQuestion3);
-		Question additionalQuestion4 = Question.of("추가4 테스트 질문입니다.", "추가 테스트 질문", QuestionType.SUSI,
-			QuestionCategory.ADMISSION_GUIDELINE);
-		questionRepository.save(additionalQuestion4);
+		Question additionalQuestionETC = Question.of("추가4 테스트 질문 예시입니다.", "추가 테스트 질문 예시", QuestionType.SUSI,
+			QuestionCategory.ETC);
+		questionRepository.save(additionalQuestionETC);
 
 		answer = Answer.of(question, "테스트 답변입니다.");
 		answerRepository.save(answer);
@@ -67,7 +61,8 @@ class QuestionRepositoryTest extends RepositoryTest {
 
 		// then
 		assertThat(questions).isNotEmpty();
-		assertThat(questions).isEqualTo(List.of(question));
+		assertThat(questions.size()).isEqualTo(4);
+		assertThat(questions.get(0)).isEqualTo(question);
 	}
 
 	@DisplayName("질문을 조회하는데 실패한 경우 - type, category를 못 찾은 경우")
@@ -95,7 +90,8 @@ class QuestionRepositoryTest extends RepositoryTest {
 
 		// then
 		assertThat(questions).isNotEmpty();
-		assertThat(questions).isEqualTo(List.of(question));
+		assertThat(questions.size()).isEqualTo(5);
+		assertThat(questions.get(0)).isEqualTo(question);
 	}
 
 	@DisplayName("질문을 조회하는데 실패한 경우 - type만으로 조회할 때")
