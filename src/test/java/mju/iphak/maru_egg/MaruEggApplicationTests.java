b6df9f17ab9c9167bce32a3689ca8256342219d7
나경호 <hoyana1225@gmail.com>
2024-07-20 14:27:07 +0900

    test: 환경변수 yml 파일에서 관리

--- src/test/java/mju/iphak/maru_egg/MaruEggApplicationTests.java
+++ src/test/java/mju/iphak/maru_egg/MaruEggApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 import mju.iphak.maru_egg.common.config.QueryDslConfig;
 import mju.iphak.maru_egg.common.config.SecurityConfig;
@@ -13,10 +12,6 @@ import mju.iphak.maru_egg.common.config.WebClientConfig;
 @Import({TestcontainersConfiguration.class, QueryDslConfig.class, WebClientConfig.class, SecurityConfig.class})
 @SpringBootTest
 @ActiveProfiles("test")
-@TestPropertySource(properties = {
-	"web-client.base-url=http://localhost:8080",
-	"JWT_SECRET=my_test_secret_key"
-})
 class MaruEggApplicationTests {
 
 	@Test
