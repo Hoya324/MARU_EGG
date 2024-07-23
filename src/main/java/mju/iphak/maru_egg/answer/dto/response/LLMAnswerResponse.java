2270a5df51d24d4b780275ed40e91f84fac6270c
나경호 <hoyana1225@gmail.com>
2024-07-23 19:52:59 +0900

    refactor: llm 질문 response에 references 추가

--- src/main/java/mju/iphak/maru_egg/answer/dto/response/LLMAnswerResponse.java
+++ src/main/java/mju/iphak/maru_egg/answer/dto/response/LLMAnswerResponse.java
@@ -1,19 +1,32 @@
 package mju.iphak.maru_egg.answer.dto.response;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import lombok.Builder;
 import mju.iphak.maru_egg.answer.domain.Answer;
 
-@Builder
 public record LLMAnswerResponse(
 	String questionType,
 	String questionCategory,
-	String answer
+	String answer,
+	List<AnswerReferenceResponse> references
 ) {
-	public static LLMAnswerResponse of(String questionType, String questionCategory, Answer answer) {
+	@Builder
+	public LLMAnswerResponse {
+		if (Objects.isNull(references)) {
+			references = new ArrayList<>();
+		}
+	}
+
+	public static LLMAnswerResponse of(String questionType, String questionCategory, Answer answer,
+		List<AnswerReferenceResponse> references) {
 		return LLMAnswerResponse.builder()
 			.questionType(questionType)
 			.questionCategory(questionCategory)
 			.answer(answer.getContent())
+			.references(references)
 			.build();
 	}
 }
