181a49963a385f8884851aa1ceb05181a0a0e22f
나경호 <hoyana1225@gmail.com>
2024-07-23 16:53:18 +0900

    refactor: QuestionCategory와 QuestionType 수정

--- src/main/java/mju/iphak/maru_egg/question/domain/QuestionCategory.java
+++ src/main/java/mju/iphak/maru_egg/question/domain/QuestionCategory.java
@@ -13,10 +13,30 @@ public enum QuestionCategory {
 	PAST_QUESTIONS("기출문제"),
 	INTERVIEW_PRACTICAL_TEST("면접/실기");
 
-	private final String questionCategory;
+	private final String category;
 
 	@Override
 	public String toString() {
-		return this.questionCategory;
+		return this.category;
+	}
+
+	public static QuestionCategory convertToCategory(String category) {
+		if (category.equals(ADMISSION_GUIDELINE.getCategory())) {
+			return ADMISSION_GUIDELINE;
+		}
+
+		if (category.equals(PASSING_RESULT.getCategory())) {
+			return PASSING_RESULT;
+		}
+
+		if (category.equals(PAST_QUESTIONS.getCategory())) {
+			return PAST_QUESTIONS;
+		}
+
+		if (category.equals(INTERVIEW_PRACTICAL_TEST.getCategory())) {
+			return INTERVIEW_PRACTICAL_TEST;
+		}
+
+		return null;
 	}
 }
