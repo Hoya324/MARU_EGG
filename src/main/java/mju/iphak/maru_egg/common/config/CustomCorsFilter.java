7cf09d887ea926ab84262a6220fdb9408885ec0b
나경호 <hoyana1225@gmail.com>
2024-08-05 22:36:05 +0900

    refactor: 텍스트 추출검사 추가

--- src/main/java/mju/iphak/maru_egg/common/config/CustomCorsFilter.java
+++ src/main/java/mju/iphak/maru_egg/common/config/CustomCorsFilter.java
@@ -29,7 +29,7 @@ public class CustomCorsFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Methods", "*");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers",
-			"Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
+			"Origin, X-Requested-With, Content-Type, Accept, Key, authorization");
 
 		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
 			response.setStatus(HttpServletResponse.SC_OK);
