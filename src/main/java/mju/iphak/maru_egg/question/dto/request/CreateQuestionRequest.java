ae20ec4502672bad14a989167cec0e5af76edabf
나경호 <hoyana1225@gmail.com>
2024-08-07 00:36:44 +0900

    feat: 질문 답변 생성 request dto

---
+++ src/main/java/mju/iphak/maru_egg/question/dto/request/CreateQuestionRequest.java
@@ -0,0 +1,28 @@
+package mju.iphak.maru_egg.question.dto.request;
+
+import static mju.iphak.maru_egg.question.utils.PhraseExtractionUtils.*;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import mju.iphak.maru_egg.answer.dto.request.CreateAnswerRequest;
+import mju.iphak.maru_egg.question.domain.Question;
+import mju.iphak.maru_egg.question.domain.QuestionCategory;
+import mju.iphak.maru_egg.question.domain.QuestionType;
+
+@Schema(description = "질문 생성 요청 DTO")
+public record CreateQuestionRequest(
+	@Schema(description = "질문 내용", example = "예시 질문입니다.")
+	String content,
+
+	@Schema(description = "질문 타입(수시, 정시, 편입학)", allowableValues = {"SUSI", "JEONGSI", "PYEONIP"})
+	QuestionType questionType,
+
+	@Schema(description = "질문 내용", allowableValues = {"ADMISSION_GUIDELINE", "PASSING_RESULT",
+		"PAST_QUESTIONS", "INTERVIEW_PRACTICAL_TEST"})
+	QuestionCategory questionCategory,
+
+	CreateAnswerRequest answer
+) {
+	public Question toEntity() {
+		return Question.of(content, extractPhrases(content), questionType, questionCategory);
+	}
+}
