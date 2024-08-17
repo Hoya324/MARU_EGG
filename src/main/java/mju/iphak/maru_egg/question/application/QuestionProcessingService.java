25a5534cbed692aa50c68606857620a35765d8dd
나경호 <hoyana1225@gmail.com>
2024-08-17 23:54:22 +0900

    refactor: 답변에 대한 역할 제거

--- src/main/java/mju/iphak/maru_egg/question/application/QuestionProcessingService.java
+++ src/main/java/mju/iphak/maru_egg/question/application/QuestionProcessingService.java
@@ -13,13 +13,10 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mju.iphak.maru_egg.answer.application.AnswerApiClient;
+import mju.iphak.maru_egg.answer.application.AnswerManager;
 import mju.iphak.maru_egg.answer.domain.Answer;
-import mju.iphak.maru_egg.answer.domain.AnswerReference;
-import mju.iphak.maru_egg.answer.dto.request.LLMAskQuestionRequest;
 import mju.iphak.maru_egg.answer.dto.response.AnswerReferenceResponse;
 import mju.iphak.maru_egg.answer.dto.response.AnswerResponse;
-import mju.iphak.maru_egg.answer.dto.response.LLMAnswerResponse;
-import mju.iphak.maru_egg.answer.repository.AnswerReferenceRepository;
 import mju.iphak.maru_egg.common.utils.NLP.TextSimilarityUtils;
 import mju.iphak.maru_egg.common.utils.PhraseExtractionUtils;
 import mju.iphak.maru_egg.question.domain.Question;
@@ -36,99 +33,28 @@ import mju.iphak.maru_egg.question.repository.QuestionRepository;
 public class QuestionProcessingService {
 
 	private static final double STANDARD_SIMILARITY = 0.95;
-	private static final String INVALID_ANSWER = "해당 내용에 대한 정보는 존재하지 않습니다. 정확한 내용은 입학지원팀에 문의해주세요.";
 
 	private final QuestionRepository questionRepository;
 	private final AnswerApiClient answerApiClient;
-	private final AnswerReferenceRepository answerReferenceRepository;
+	private final AnswerManager answerManager;
 
 	public QuestionResponse question(final QuestionType type, final QuestionCategory category, final String content) {
 		String contentToken = PhraseExtractionUtils.extractPhrases(content);
-		List<QuestionCore> questionCores = getQuestionCores(type, category, contentToken);
 
+		List<QuestionCore> questionCores = getQuestionCores(type, category, contentToken);
 		if (questionCores.isEmpty()) {
 			log.info("저장된 질문이 없어 새롭게 LLM서버에 질문을 요청합니다.");
-			return processNewQuestion(type, category, content, contentToken);
+			return answerManager.processNewQuestion(type, category, content, contentToken);
 		}
 
 		Long mostSimilarQuestionId = findMostSimilarQuestionId(questionCores, contentToken);
-
 		if (mostSimilarQuestionId != null) {
 			log.info("유사한 질문이 있어 DB에서 질문을 재사용합니다.");
 			return getExistingQuestionResponse(mostSimilarQuestionId);
 		}
 
 		log.info("유사한 질문이 없어 새롭게 LLM서버에 질문을 요청합니다.");
-		return processNewQuestion(type, category, content, contentToken);
-	}
-
-	private QuestionResponse processNewQuestion(QuestionType type, QuestionCategory category, String content,
-		String contentToken) {
-		LLMAskQuestionRequest askQuestionRequest = LLMAskQuestionRequest.of(
-			type.getType(),
-			category == null ? "" : category.getCategory(),
-			content
-		);
-
-		LLMAnswerResponse llmAnswerResponse = answerApiClient.askQuestion(askQuestionRequest).block();
-
-		if (!isValidAnswer(llmAnswerResponse)) {
-			return QuestionResponse.valueOfInvalidQuestion(content);
-		}
-
-		Question newQuestion = saveQuestion(type,
-			QuestionCategory.convertToCategory(llmAnswerResponse.questionCategory()), content, contentToken);
-		Answer newAnswer = saveAnswer(newQuestion, llmAnswerResponse.answer());
-		saveAnswerReferences(newAnswer, llmAnswerResponse.references());
-
-		return createQuestionResponse(newQuestion, newAnswer, llmAnswerResponse.references());
-	}
-
-	private boolean isValidAnswer(LLMAnswerResponse llmAnswerResponse) {
-		return !llmAnswerResponse.answer().equals(INVALID_ANSWER);
-	}
-
-	private void saveAnswerReferences(Answer answer, List<AnswerReferenceResponse> referenceResponses) {
-		List<AnswerReference> answerReferences = referenceResponses.stream()
-			.map(reference -> AnswerReference.of(reference.title(), reference.link(), answer))
-			.toList();
-		answerReferenceRepository.saveAll(answerReferences);
-	}
-
-	private Answer saveAnswer(Question question, String content) {
-		Answer newAnswer = Answer.of(question, content);
-		return answerApiClient.saveAnswer(newAnswer);
-	}
-
-	private Question saveQuestion(QuestionType type, QuestionCategory category, String content, String contentToken) {
-		Question question = Question.of(content, contentToken, type, category);
-		return questionRepository.save(question);
-	}
-
-	private Long findMostSimilarQuestionId(List<QuestionCore> questionCores, String contentToken) {
-		Map<CharSequence, Integer> tfIdf1 = computeTfIdf(questionCores, contentToken);
-
-		return questionCores.stream()
-			.map(core -> {
-				Map<CharSequence, Integer> tfIdf2 = computeTfIdf(questionCores, core.contentToken());
-				double similarity = TextSimilarityUtils.computeCosineSimilarity(tfIdf1, tfIdf2);
-				return similarity > STANDARD_SIMILARITY ? core.id() : null;
-			})
-			.filter(id -> id != null)
-			.findFirst()
-			.orElse(null);
-	}
-
-	private Map<CharSequence, Integer> computeTfIdf(List<QuestionCore> questionCores, String contentToken) {
-		List<String> contentTokens = questionCores.stream()
-			.map(QuestionCore::contentToken)
-			.toList();
-
-		try {
-			return TextSimilarityUtils.computeTfIdf(contentTokens, contentToken);
-		} catch (Exception e) {
-			throw new RuntimeException(String.format(INTERNAL_ERROR_TEXT_SIMILARITY.getMessage()));
-		}
+		return answerManager.processNewQuestion(type, category, content, contentToken);
 	}
 
 	private List<QuestionCore> getQuestionCores(QuestionType type, QuestionCategory category, String contentToken) {
@@ -159,5 +85,31 @@ public class QuestionProcessingService {
 		List<AnswerReferenceResponse> answerReferenceResponses) {
 		AnswerResponse answerResponse = AnswerResponse.from(answer);
 		return QuestionResponse.of(question, answerResponse, answerReferenceResponses);
+	}
+
+	public Long findMostSimilarQuestionId(List<QuestionCore> questionCores, String contentToken) {
+		Map<CharSequence, Integer> tfIdf1 = computeTfIdf(questionCores, contentToken);
+
+		return questionCores.stream()
+			.map(core -> {
+				Map<CharSequence, Integer> tfIdf2 = computeTfIdf(questionCores, core.contentToken());
+				double similarity = TextSimilarityUtils.computeCosineSimilarity(tfIdf1, tfIdf2);
+				return similarity > STANDARD_SIMILARITY ? core.id() : null;
+			})
+			.filter(id -> id != null)
+			.findFirst()
+			.orElse(null);
+	}
+
+	private Map<CharSequence, Integer> computeTfIdf(List<QuestionCore> questionCores, String contentToken) {
+		List<String> contentTokens = questionCores.stream()
+			.map(QuestionCore::contentToken)
+			.toList();
+
+		try {
+			return TextSimilarityUtils.computeTfIdf(contentTokens, contentToken);
+		} catch (Exception e) {
+			throw new RuntimeException(String.format(INTERNAL_ERROR_TEXT_SIMILARITY.getMessage()));
+		}
 	}
 }
