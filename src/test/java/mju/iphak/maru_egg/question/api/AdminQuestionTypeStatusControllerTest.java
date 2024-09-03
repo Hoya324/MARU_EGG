9759a19b5f2b5c78ccb197a5c87bb28b65a16132
나경호 <hoyana1225@gmail.com>
2024-09-03 11:45:11 +0900

    test: 질문 상태 목록 분리(Admin과 일반 유저 분리)

--- src/test/java/mju/iphak/maru_egg/question/api/AdminQuestionTypeStatusControllerTest.java
+++ src/test/java/mju/iphak/maru_egg/question/api/AdminQuestionTypeStatusControllerTest.java
@@ -7,7 +7,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -26,7 +25,6 @@ class AdminQuestionTypeStatusControllerTest extends IntegrationTest {
 
 	@BeforeEach
 	void setUp() {
-		MockitoAnnotations.openMocks(this);
 		questionTypeStatusService.initializeQuestionTypeStatus();
 		questionTypeStatusService.deleteQuestionTypeStatus(QuestionType.JEONGSI);
 	}
