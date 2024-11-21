893615e7ca3b1e544a93f06cce41ee29551818ea
나경호 <hoyana1225@gmail.com>
2024-11-21 21:35:44 +0900

    test: delete question 분리

---
+++ src/test/java/mju/iphak/maru_egg/question/application/delete/DeleteQuestionServiceTest.java
@@ -0,0 +1,94 @@
+package mju.iphak.maru_egg.question.application.delete;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import jakarta.persistence.EntityNotFoundException;
+import mju.iphak.maru_egg.admission.domain.AdmissionCategory;
+import mju.iphak.maru_egg.admission.domain.AdmissionType;
+import mju.iphak.maru_egg.answer.application.find.FindAnswerByQuestionIdService;
+import mju.iphak.maru_egg.answer.domain.Answer;
+import mju.iphak.maru_egg.answer.repository.AnswerRepository;
+import mju.iphak.maru_egg.common.MockTest;
+import mju.iphak.maru_egg.question.dao.request.QuestionCoreDAO;
+import mju.iphak.maru_egg.question.dao.response.QuestionCore;
+import mju.iphak.maru_egg.question.domain.Question;
+import mju.iphak.maru_egg.question.repository.QuestionRepository;
+
+class DeleteQuestionServiceTest extends MockTest {
+
+	@Mock
+	private FindAnswerByQuestionIdService findAnswerByQuestionId;
+
+	@Mock
+	private QuestionRepository questionRepository;
+
+	@Mock
+	private AnswerRepository answerRepository;
+
+	@InjectMocks
+	private DeleteQuestionService deleteQuestion;
+
+	private Question question;
+	private Answer answer;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+		question = mock(Question.class);
+		answer = mock(Answer.class);
+
+		when(question.getId()).thenReturn(1L);
+		when(answer.getId()).thenReturn(1L);
+		when(findAnswerByQuestionId.invoke(1L)).thenReturn(answer);
+		when(answerRepository.findByQuestionId(anyLong())).thenReturn(Optional.of(answer));
+		when(questionRepository.searchQuestions(any(QuestionCoreDAO.class)))
+			.thenReturn(Optional.of(List.of(QuestionCore.of(1L, "테스트 질문입니다."))));
+		when(questionRepository.findById(1L)).thenReturn(Optional.of(question));
+	}
+
+	@DisplayName("질문 삭제에 성공한 경우")
+	@Test
+	void 질문_삭제_성공() {
+		// given
+		Long id = 1L;
+		Question question = Question.of("질문", "질문", AdmissionType.SUSI, AdmissionCategory.ADMISSION_GUIDELINE);
+		when(questionRepository.findById(id)).thenReturn(Optional.ofNullable(question));
+
+		// when
+		deleteQuestion.invoke(id);
+
+		// then
+		verify(questionRepository, times(1)).findById(id);
+		verify(questionRepository, times(1)).delete(any(Question.class));
+	}
+
+	@DisplayName("질문 삭제시에 조회 실패")
+	@Test
+	public void 질문_삭제시_조회_실패_NOTFOUND() {
+		// given
+		Long id = 1L;
+		when(questionRepository.findById(id)).thenReturn(Optional.empty());
+
+		// when & then
+		EntityNotFoundException exception = assertThrows(EntityNotFoundException.class, () -> {
+			deleteQuestion.invoke(id);
+		});
+
+		assertThat("id: 1인 질문을 찾을 수 없습니다.").isEqualTo(exception.getMessage());
+	}
+
+}
