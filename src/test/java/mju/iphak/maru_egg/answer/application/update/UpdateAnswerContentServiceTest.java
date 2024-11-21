eaa7a026c00b6caed57cb4b99d47c5d4729d3d63
나경호 <hoyana1225@gmail.com>
2024-11-21 21:47:13 +0900

    test: Update Answer Content 분리

---
+++ src/test/java/mju/iphak/maru_egg/answer/application/update/UpdateAnswerContentServiceTest.java
@@ -0,0 +1,47 @@
+package mju.iphak.maru_egg.answer.application.update;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import jakarta.persistence.EntityNotFoundException;
+import mju.iphak.maru_egg.answer.repository.AnswerRepository;
+import mju.iphak.maru_egg.common.MockTest;
+
+class UpdateAnswerContentServiceTest extends MockTest {
+
+	@Mock
+	private AnswerRepository answerRepository;
+
+	@InjectMocks
+	private UpdateAnswerContentService updateAnswerContent;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@DisplayName("[실패] 답변 내용을 수정할 때, 답변을 찾을 수 없으면 예외가 발생한다.")
+	@Test
+	void 답변_내용_수정_실패_NOTFOUND() {
+		// given
+		Long id = 1L;
+		when(answerRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+		// when & then
+		EntityNotFoundException exception = assertThrows(EntityNotFoundException.class, () -> {
+			updateAnswerContent.invoke(id, "새로운 내용");
+		});
+
+		assertThat(exception.getMessage()).isEqualTo("답변 id가 1인 답변을 찾을 수 없습니다.");
+	}
+}
