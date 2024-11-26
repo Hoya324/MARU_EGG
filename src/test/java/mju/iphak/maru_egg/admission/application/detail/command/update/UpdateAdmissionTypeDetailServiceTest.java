cdbb08bdc8d8a1f1fa2a91c18d2f78ac2bcb08ca
나경호 <hoyana1225@gmail.com>
2024-11-26 14:39:09 +0900

    test: AdmissionType Detail Mock Test Convention 적용

--- src/test/java/mju/iphak/maru_egg/admission/application/detail/command/update/UpdateAdmissionTypeDetailServiceTest.java
+++ src/test/java/mju/iphak/maru_egg/admission/application/detail/command/update/UpdateAdmissionTypeDetailServiceTest.java
@@ -14,8 +14,9 @@ import org.mockito.MockitoAnnotations;
 
 import jakarta.persistence.EntityNotFoundException;
 import mju.iphak.maru_egg.admission.repository.AdmissionTypeDetailRepository;
+import mju.iphak.maru_egg.common.MockTest;
 
-class UpdateAdmissionTypeDetailServiceTest {
+class UpdateAdmissionTypeDetailServiceTest extends MockTest {
 
 	@Mock
 	private AdmissionTypeDetailRepository admissionTypeDetailRepository;
@@ -28,15 +29,18 @@ class UpdateAdmissionTypeDetailServiceTest {
 		MockitoAnnotations.openMocks(this);
 	}
 
-	@DisplayName("상세 정보 업데이트 실패 - 존재하지 않는 ID")
+	@DisplayName("[실패] 상세 정보 업데이트 요청 - 존재하지 않는 ID")
 	@Test
-	void invoke_Failure_NotFound() {
+	void 상세정보_업데이트_실패_존재하지_않는_ID() {
 		// given
 		Long invalidId = 999L;
 		when(admissionTypeDetailRepository.findById(invalidId)).thenReturn(Optional.empty());
 
 		// when & then
-		assertThrows(EntityNotFoundException.class,
+		EntityNotFoundException exception = assertThrows(EntityNotFoundException.class,
 			() -> updateAdmissionTypeDetail.invoke(invalidId, "newName"));
+
+		// then
+		assertEquals("id가 999인 입학 전형 상세를 찾을 수 없습니다.", exception.getMessage());
 	}
 }
