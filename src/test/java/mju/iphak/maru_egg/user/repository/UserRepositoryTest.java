488275bb238c0355021fdb4e35550fa662b5090b
나경호 <hoyana1225@gmail.com>
2024-11-26 16:57:14 +0900

    test: User Repository Test Convention 적용

--- src/test/java/mju/iphak/maru_egg/user/repository/UserRepositoryTest.java
+++ src/test/java/mju/iphak/maru_egg/user/repository/UserRepositoryTest.java
@@ -3,6 +3,7 @@ package mju.iphak.maru_egg.user.repository;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -21,18 +22,34 @@ public class UserRepositoryTest extends RepositoryTest {
 	@BeforeEach
 	public void setUp() throws Exception {
 		email = "maru@gmail.com";
-		saveUser = userRepository.save(User.builder().email(email).role(Role.GUEST).build());
+		saveUser = saveUser(email);
 	}
 
+	@DisplayName("[성공] 이메일이 존재하는 경우 true를 반환")
 	@Test
-	public void existsByEmail_존재하는경우_true() {
-		final boolean existsByEmail = userRepository.findByEmail(email).isPresent();
+	void 이메일_존재_true() {
+		// when
+		boolean existsByEmail = checkIfEmailExists(email);
+
+		// then
 		assertThat(existsByEmail).isTrue();
 	}
 
+	@DisplayName("[실패] 이메일이 존재하지 않는 경우 false를 반환")
 	@Test
-	public void existsByEmail_존재하지않은_경우_false() {
-		final boolean existsByEmail = userRepository.findByEmail("invalid@asd.com").isPresent();
+	void 이메일_존재하지않음_false() {
+		// when
+		boolean existsByEmail = checkIfEmailExists("invalid@asd.com");
+
+		// then
 		assertThat(existsByEmail).isFalse();
+	}
+
+	private User saveUser(String email) {
+		return userRepository.save(User.builder().email(email).role(Role.GUEST).build());
+	}
+
+	private boolean checkIfEmailExists(String email) {
+		return userRepository.findByEmail(email).isPresent();
 	}
 }
